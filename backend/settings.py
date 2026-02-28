cd0ae3ee34c604fc25c375e55201bd7a3ca12cbb
Guillem Leon Font <guillem.leonf@gmail.com>
2026-02-28 23:49:48 +0100

    Fixed bad connection to database

--- backend/settings.py
+++ backend/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
     "default": dj_database_url.config(
         env="DATABASE_URL",
         conn_max_age=600,
-        ssl_require=not DEBUG,
+        ssl_require=True,
     )
 }
 
@@ -131,10 +131,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
-    "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": False,
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 
 # AWS S3 CONFIG
